d975856e1c0d7bdba59b639dc0d53b4e6059ac8b
YSawc <niceage6690@gmail.com>
2019-09-08 21:46:11 +0900

    move to div__IDEA

--- src/popup.js
+++ src/popup.js
@@ -1,46 +1,3 @@
-// Tip:
-// input blog::hoge (PARENT_DIT/blog/hoge)
-// blog: parent_node
-// hoge: children_file
-
-// blog .. parent_dir
-// hoge: children_node (id: .., parent_node: .., name: ..)
-
-/*
- * TIP:
- *
- * func ..
- *
- * create
- *  parse ..
- *		to parentFileDir
- *		to bookmarkFileDir
- * undo :: undo recent_createBookMark
- * */
-
-// NODE:
-// parent_dir
-
-// if (enter_input) {
-//   chrome.bookmarks.create();
-//   if ( func__parse_inputText(TEXT) = parent ) {
-//     call:func__parse_to_parent_dir;
-//   } else if (func__parse_inputText(TEXT) = lastChild) {
-//     call:func__parse_to_dir;
-//   }
-// }
-
-// func__parse_to_parent_dir() {
-// }
-
-// func__parse_to_dir() {
-// }
-
-// func__parse_inputText(string s) {
-//   return parsed_dir
-// }
-
-
 /*
  * INIT:
  * */
@@ -170,3 +127,48 @@ function clearText() {
 /*
  * IDEA
  * */
+
+// Tip:
+// input blog::hoge (PARENT_DIT/blog/hoge)
+// blog: parent_node
+// hoge: children_file
+
+// blog .. parent_dir
+// hoge: children_node (id: .., parent_node: .., name: ..)
+
+/*
+ * TIP:
+ *
+ * func ..
+ *
+ * create
+ *  parse ..
+ *		to parentFileDir
+ *		to bookmarkFileDir
+ * undo :: undo recent_createBookMark
+ * */
+
+// NODE:
+// parent_dir
+
+// if (enter_input) {
+//   chrome.bookmarks.create();
+//   if ( func__parse_inputText(TEXT) = parent ) {
+//     call:func__parse_to_parent_dir;
+//   } else if (func__parse_inputText(TEXT) = lastChild) {
+//     call:func__parse_to_dir;
+//   }
+// }
+
+// func__parse_to_parent_dir() {
+// }
+
+// func__parse_to_dir() {
+// }
+
+// func__parse_inputText(string s) {
+//   return parsed_dir
+// }
+
+
+

95fa0c43777cbe4673144d0ef36da4bd38b14bda
YSawc <niceage6690@gmail.com>
2019-09-08 09:46:48 +0900

    fix to work

--- popup.js
+++ popup.js
@@ -134,7 +134,7 @@ function undo() {
 
 function redo() {
 	chrome.bookmarks.create(
-		{ parentId: redoArr[0][0], title: redoArr[0][1], url: redoArr[0][2] },
+		{ 'parentId': redoArr[0]['parentId'], 'title': redoArr[0]['title'], 'url': redoArr[0]['url'] },
 	);
 };
 

76511e21d6c6293eef2f7fa4690aede051944685
YSawc <niceage6690@gmail.com>
2019-12-23 08:22:01 +0900

    Delete small

--- src/popup.js
+++ src/popup.js
@@ -1,34 +1,11 @@
-/*
- * INIT:
- * */
-
-// initNodeNum:: default: "1"
 const initNodeNum = "1"
-// RECENTGETNUM:: const: 1
 const RECENTGETNUM = 1
 let redoArr = Array(RECENTGETNUM)
 
-/*
- * NUM:
- * */
-
 const ZERO = 0
-// int: recentID .. for func__UNDO
 recentID = chrome.bookmarks.getRecent(RECENTGETNUM, function(r){r[ZERO].id});
 
-/*
- * MAIN:
- * */
-
-// key input trigger
 $(document).on("keypress", "input", function(e){
-	// TODO: ctrl+[ work same to ESC
-	// if(event.ctrlKey) {
-	//   if(e.whitch === 219) {
-	//     console.log('ctrl+219 input');
-	//   };
-	// };
-
 	// detect ENTER_input
 	if(e.which == 13){
 		let inputVal = $(this).val();
@@ -48,28 +25,17 @@ $(document).on("keypress", "input", function(e){
 // now support only oneDir of nesting bookMark
 function parseToDir(str) {
 	let splitStr = str.split('::');
-
-  // dirBookMark: bookmark
 	let dirBookMark = splitStr.pop();
-	// console.log(dirBookMark);
-
-  // dirParent: for search create Dir of bookMark
 	let dirParent = splitStr;
-	// console.log(dirParent);
-	// console.log(splitStr);
 
 	// TODO:
 	// rewite to recursive_func
 	chrome.bookmarks.search(
 		String(dirParent),
 		function(r) {
-			// console.log(r.length);
-			// const constParentId = r["0"].id;
-			// console.log(r);
 			for (let i=0; i < r.length; i++ ) {
 				if (r[i.toString()].title === String(dirParent)) {
 					const dirParentID = r[i.toString()].id;
-					// console.log(String(dirParentID));
 					createBookMark(dirParentID, String(dirBookMark), Data.URL);
 					return true;
 				};
@@ -78,7 +44,6 @@ function parseToDir(str) {
 	);
 }
 
-// TODO: Refactoring
 function createBookMark(parentId, title, url) {
 	chrome.bookmarks.create({
 		'parentId': parentId,
@@ -87,31 +52,11 @@ function createBookMark(parentId, title, url) {
 	});
 }
 
-function parseToParendDir() {
-	// TODO
-}
-
-function parseToLastChildDir() {
-	// TODO
-}
-
-function parseInputText(str) {
-	// TODO:
-	// return (list_of_parsedTEXT)
-}
-
-$('#save').click(function() {
-	// TODO:
-	// create bookmark
-});
-
 function undo() {
 	chrome.bookmarks.getRecent(
 		RECENTGETNUM,
 		function(res){
-			// for func__redo
 			redoArr[ZERO] = {'parentId': res[ZERO].parentId, 'title': res[ZERO].title, 'url': res[ZERO].url};
-
 			chrome.bookmarks.remove(String(res[ZERO].id))
 		}
 	);
@@ -123,11 +68,6 @@ function redo() {
 	);
 };
 
-function clearText() {
-	// TODO:
-	// crera TextBox
-}
-
 /*
  * DEBUG
  * */
@@ -135,49 +75,3 @@ function clearText() {
 // chrome.bookmarks.getTree(function(r){console.log(r);});
 // chrome.bookmarks.getChildren(initNodeNum, function(r){console.log(r);});
 // chrome.bookmarks.getRecent(RECENTGETNUM, function(r){console.log(r);});
-
-/*
- * IDEA
- * */
-
-// Tip:
-// input blog::hoge (PARENT_DIT/blog/hoge)
-// blog: parent_node
-// hoge: children_file
-
-// blog .. parent_dir
-// hoge: children_node (id: .., parent_node: .., name: ..)
-
-/*
- * TIP:
- * */
-
-// func ..
-
-// create
-//  parse ..
-//    to parentFileDir
-//    to bookmarkFileDir
-// undo :: undo recent_createBookMark
-
-// NODE:
-// parent_dir
-
-// if (enter_input) {
-//   chrome.bookmarks.create();
-//   if ( func__parse_inputText(TEXT) = parent ) {
-//     call:func__parse_to_parent_dir;
-//   } else if (func__parse_inputText(TEXT) = lastChild) {
-//     call:func__parse_to_dir;
-//   }
-// }
-
-// func__parse_to_parent_dir() {
-// }
-
-// func__parse_to_dir() {
-// }
-
-// func__parse_inputText(string s) {
-//   return parsed_dir
-// }
